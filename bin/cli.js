155c9ed354da95981146276d87ae0e7752e27421
jpurvis2 <jpurvis2@email.unc.edu>
2022-11-05 13:43:56 -0400

    A03 project2

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import roll from "../lib/roll.js"
+import { roll } from "../lib/roll.js"
 const args = minimist(process.argv.slice(2))
 
 if (!args.sides || !args.dice ||!args.rolls){
